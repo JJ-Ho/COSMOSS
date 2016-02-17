c404212fec05ef4deae7201cca53037893c56c52
Jia-Jung Ho <jhoatgit@gmail.com>
2016-02-17 11:15:30 -0600

    Update Main version number

--- Main_GUI/GUI_COSMOSS.m
+++ Main_GUI/GUI_COSMOSS.m
@@ -2,7 +2,7 @@ function GUI = GUI_COSMOSS(hMainFig)
 % This function will build all GUI elements on pre-build GUI base figure
 
 %% Pre-setting
-Version = '1.0.2';
+Version = '1.0.3';
 
 %% Add base layout
 MainLayout = uix.VBoxFlex(...

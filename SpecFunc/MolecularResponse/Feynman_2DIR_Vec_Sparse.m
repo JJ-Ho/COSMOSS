29ba6d92ff272d1c3d6f7493f570ea8fbf5fe23d
Jia-Jung Ho <jhoatgit@gmail.com>
2017-01-10 16:17:00 -0600

    New 243 => 1 pathway cutoff test on 2DIR code success Need to add GUI input for the cutoff

--- SpecFunc/MolecularResponse/Feynman_2DIR_Vec_Sparse.m
+++ SpecFunc/MolecularResponse/Feynman_2DIR_Vec_Sparse.m
@@ -1,4 +1,4 @@
-function [Grid,Freq,Int,Index] = Feynman_2DIR_Vec_Sparse(FreqRange,EJR,F1,F2,M_Ex_01,M_Ex_12)
+function [Grid,Freq,Int,Index] = Feynman_2DIR_Vec_Sparse(FreqRange,EJR,F1,F2,M01,M12)
 % 
 % This function generate Feynman pathway of 2DSFG with given polarization.
 % 
@@ -27,8 +27,8 @@ function [Grid,Freq,Int,Index] = Feynman_2DIR_Vec_Sparse(FreqRange,EJR,F1,F2,M_E
 % TwoDIR = Data_COSMOSS.TwoDIR;
 % F1 = TwoDIR.H.Sort_Ex_F1;
 % F2 = TwoDIR.H.Sort_Ex_F2;
-% M_Ex_01 = TwoDIR.Mu.M_Ex_01;
-% M_Ex_12 = TwoDIR.Mu.M_Ex_12;
+% M01 = TwoDIR.Mu.M_Ex_01;
+% M12 = TwoDIR.Mu.M_Ex_12;
 % 
 % % M_Ex_01(abs(M_Ex_01)<1e-10) = 0;
 % % M_Ex_12(abs(M_Ex_12)<1e-10) = 0;
@@ -37,6 +37,9 @@ function [Grid,Freq,Int,Index] = Feynman_2DIR_Vec_Sparse(FreqRange,EJR,F1,F2,M_E
 F1 = round(F1);
 F2 = round(F2);
 
+% Merge the first two index of Mu_Ex into one => 2D verion Mu_Ex
+M12 = reshape(M12,[],3);
+
 %% Prepare index
 % Find number of modes in 1ex and 2ex
 N1 = length(F1);
@@ -44,204 +47,271 @@ N2 = length(F2);
 
 % R1 R2 NR1 NR2 index expansion,size N^2 
 [Ib,Ia] = ndgrid(1:N1,1:N1);
+Ia = Ia(:);
+Ib = Ib(:);
+
 % XYZ index expansion, size 3^4
 [Jd,Jc,Jb,Ja] = ndgrid(1:3,1:3,1:3,1:3);
+Ja = Ja(:);
+Jb = Jb(:);
+Jc = Jc(:);
+Jd = Jd(:);
+
 % R3 NR3 index expansion, size: N^3*(N+1)/2
 [Kx,Kb,Ka] = ndgrid(1:N2,1:N1,1:N1);
-
-%[length(Ib(:)),length(Kx(:))] %% Debug
-
-%% Reduce mode number base on the transition intensity
-Cut_Off_01 = 0;
-Cut_Off_12 = 0;
-% Cut_Off_01 = 0;
-% Cut_Off_12 = 0;
-
-% 01
-Norm_M_01  = sqrt(sum(M_Ex_01.^2,2));
-
-Max_Norm_M_01 = max(Norm_M_01);
-Ind_Norm_M_01 = Norm_M_01 >= Cut_Off_01 * Max_Norm_M_01;
-
-[Mask_Ib,Mask_Ia] = ndgrid(Ind_Norm_M_01,Ind_Norm_M_01);
-Ib = Mask_Ib.*Ib;
-Ia = Mask_Ia.*Ia;
-
-Ib = Ib(Ib>0);
-Ia = Ia(Ia>0);
-
-% 12
-Norm_M_12  = sqrt(sum(M_Ex_12.^2,3));
-
-Max_Norm_M_12 = max(Norm_M_12(:));
-
-Ind_Norm_M_12 = Norm_M_12 >= Cut_Off_12 * Max_Norm_M_12;
-
-Mask_Kx = repmat(Ind_Norm_M_12', 1,1,N1);
-Mask_Kb = repmat(Ind_Norm_M_01',N2,1,N1);
-Mask_Ka = permute(Mask_Kb,[1,3,2]);
-
-Kx = Mask_Kx.*Mask_Kb.*Mask_Ka.*Kx;
-Kb = Mask_Kx.*Mask_Kb.*Mask_Ka.*Kb;
-Ka = Mask_Kx.*Mask_Kb.*Mask_Ka.*Ka;
-
-Kx(Kx==0) = NaN;
-Kb(Kb==0) = NaN;
-Ka(Ka==0) = NaN;
+Ka = Ka(:);
+Kb = Kb(:);
+Kx = Kx(:);
 
 % Indexes of 2D version Mu_Ex, 
 % Note [N,N*(N+1)/2] is the first two Dimension of Mu_Ex
-ind_ax = sub2ind([N1,N2],Ka(:),Kx(:));
-ind_bx = sub2ind([N1,N2],Kb(:),Kx(:));
+Iax = sub2ind([N1,N2],Ka,Kx);
+Ibx = sub2ind([N1,N2],Kb,Kx);
 
-ind_ax(isnan(ind_ax)) = [];
-ind_bx(isnan(ind_bx)) = [];
+%% Reduce mode number base on the transition intensity
+Intensity_CutOff = 1e-5;
 
-% ind_xa = ind_ax;
-% ind_xb = ind_bx;
+Norm_M_01 = sqrt(sum(M01.^2,2));
+Norm_M_12 = sqrt(sum(M12.^2,2));
 
-Kx(isnan(Kx)) = [];
-Kb(isnan(Kb)) = [];
-Ka(isnan(Ka)) = [];
+% Response intensity for (GB SE)
+MI_0a = Norm_M_01(Ia);
+MI_0b = Norm_M_01(Ib);
+MI_a0 = MI_0a;
+MI_b0 = MI_0b;
 
-%[length(Ib(:)),length(Kx(:))] %% Debug
+I_R1  = MI_b0.*MI_0b.*MI_a0.*MI_0a;
+I_R2  = MI_b0.*MI_a0.*MI_0b.*MI_0a;
+I_NR1 = MI_b0.*MI_0b.*MI_a0.*MI_0a;
+I_NR2 = MI_a0.*MI_b0.*MI_0b.*MI_0a;
 
-%% Get 2DIR response from index version kronec product
-% R1 R2 NR1 NR2
-M_0a = M_Ex_01(Ia(:),:);
-M_0b = M_Ex_01(Ib(:),:);
-M_a0 = M_0a;
-M_b0 = M_0b;
+% Response intensity for EA
+NI_0a = Norm_M_01(Ka);
+NI_0b = Norm_M_01(Kb);
 
-% Response (GB SE)
-S_R1  = (M_b0(:,Ja(:)).*M_0b(:,Jb(:)).*M_a0(:,Jc(:)).*M_0a(:,Jd(:)))*EJR';
-S_R2  = (M_b0(:,Ja(:)).*M_a0(:,Jb(:)).*M_0b(:,Jc(:)).*M_0a(:,Jd(:)))*EJR';
-S_NR1 = (M_b0(:,Ja(:)).*M_0b(:,Jb(:)).*M_a0(:,Jc(:)).*M_0a(:,Jd(:)))*EJR';
-S_NR2 = (M_a0(:,Ja(:)).*M_b0(:,Jb(:)).*M_0b(:,Jc(:)).*M_0a(:,Jd(:)))*EJR';
+NI_ax = Norm_M_12(Iax);
+NI_bx = Norm_M_12(Ibx);
+NI_xa = NI_ax; % ind_xa = ind_ax
+NI_xb = NI_bx; % ind_xb = ind_bx
 
-%% R3 NR3
-% Merge the first two index of Mu_Ex into one => 2D verion Mu_Ex
-TD_M_Ex = reshape(M_Ex_12,[],3);
+I_R3  = NI_xa.*NI_bx.*NI_0b.*NI_0a;
+I_NR3 = NI_xb.*NI_ax.*NI_0b.*NI_0a;
 
+% Apply CutOff
+R1_Max  = max(I_R1);
+R2_Max  = max(I_R2);
+R3_Max  = max(I_R3);
+NR1_Max = max(I_NR1);
+NR2_Max = max(I_NR2);
+NR3_Max = max(I_NR3);
+
+PI_R1  = I_R1  >= (Intensity_CutOff *  R1_Max);
+PI_R2  = I_R2  >= (Intensity_CutOff *  R2_Max);
+PI_R3  = I_R3  >= (Intensity_CutOff *  R3_Max);
+PI_NR1 = I_NR1 >= (Intensity_CutOff * NR1_Max);
+PI_NR2 = I_NR2 >= (Intensity_CutOff * NR2_Max);
+PI_NR3 = I_NR3 >= (Intensity_CutOff * NR3_Max);
+
+%% Get 2DIR response from index version kronec product(GB SE)
+% R1
+Ib_R1   = Ib(PI_R1);
+Ia_R1   = Ia(PI_R1);
+S_R1    = ( M01(Ib_R1 ,Ja).*M01(Ib_R1 ,Jb).*M01(Ia_R1 ,Jc).*M01(Ia_R1 ,Jd) )*EJR';
+
+Ea_1    = F1(Ia_R1);
+Eb_1    = F1(Ib_R1);
+Freq_R1 = [ Ea_1, zeros(size(Ea_1)), Eb_1]; % [pump,diff,probe]
+
+% R2
+Ib_R2   = Ib(PI_R2);
+Ia_R2   = Ia(PI_R2);
+S_R2    = ( M01(Ib_R2 ,Ja).*M01(Ia_R2 ,Jb).*M01(Ib_R2 ,Jc).*M01(Ia_R2 ,Jd) )*EJR';
+
+Ea_2    = F1(Ia_R2);
+Eb_2    = F1(Ib_R2);
+Freq_R2 = [ Ea_2, Eb_2-Ea_2, Eb_2]; % [pump,diff,probe]
+
+% NR1
+Ib_NR1   = Ib(PI_NR1);
+Ia_NR1   = Ia(PI_NR1);
+S_NR1    = ( M01(Ib_NR1,Ja).*M01(Ib_NR1,Jb).*M01(Ia_NR1,Jc).*M01(Ia_NR1,Jd) )*EJR';
+
+Ea_4     = F1(Ia_NR1);
+Eb_4     = F1(Ib_NR1);
+Freq_NR1 = [ Ea_4, zeros(size(Ea_4)), Eb_4]; % [pump,diff,probe]
+
+% NR2
+Ib_NR2   = Ib(PI_NR2);
+Ia_NR2   = Ia(PI_NR2);
+S_NR2    = ( M01(Ia_NR2,Ja).*M01(Ib_NR2,Jb).*M01(Ib_NR2,Jc).*M01(Ia_NR2,Jd) )*EJR';
+
+Ea_5     = F1(Ia_NR2);
+Eb_5     = F1(Ib_NR2);
+Freq_NR2 = [ Ea_5, Ea_5-Eb_5, Ea_5]; % [pump,diff,probe]
+% This is a interesting term! only contribute to diagonal!
+
+%% Prep for R3/NR3
 % estimate of largest array size and break it down to several for loop
-MEM_CutOff = 0.1; %[GB]
+MEM_CutOff = 1e-1; %[GB]
 Ele_Max = round(MEM_CutOff/(81 * 8 / 1e9)) + 1; % number of elements to reach MEM_CufOff
-N3 = numel(Ka);
 
-if N3 > Ele_Max
+%% R3
+Iax_R3 = Iax(PI_R3);
+Ibx_R3 = Ibx(PI_R3);
+Ib_R3  =  Kb(PI_R3);
+Ia_R3  =  Ka(PI_R3);
+
+N_R3 = numel(Ia_R3);
+
+if N_R3 > Ele_Max
     % Add NaNs so I can reshape indexes
-    Padding_L = Ele_Max - mod(N3,Ele_Max);
+    Padding_L = Ele_Max - mod(N_R3,Ele_Max);
     Padding_NaN = nan(Padding_L,1);
     
-    Ka_L   = reshape( [Ka(:); Padding_NaN],Ele_Max,[]);
-    Kb_L   = reshape( [Kb(:); Padding_NaN],Ele_Max,[]);
-    ind_ax = reshape([ind_ax; Padding_NaN],Ele_Max,[]);
-    ind_bx = reshape([ind_bx; Padding_NaN],Ele_Max,[]);
+    Ia_R3_L  = reshape([ Ia_R3; Padding_NaN],Ele_Max,[]);
+    Ib_R3_L  = reshape([ Ib_R3; Padding_NaN],Ele_Max,[]);
+    Iax_R3_L = reshape([Iax_R3; Padding_NaN],Ele_Max,[]);
+    Ibx_R3_L = reshape([Ibx_R3; Padding_NaN],Ele_Max,[]);
 
-    Loop_N = size(Ka_L,2);
-
+    Loop_N = size(Ia_R3_L,2);
     % display how many loop is going to be done
-    MEM = numel(Ka_L) * 81 * 8 / 1e9; % roughly unit in GB
+    MEM = numel(Ia_R3_L) * 81 * 8 / 1e9; % roughly unit in GB
     disp('--------------------------------------')
     disp(['Memory cut-off set at ', num2str(MEM_CutOff), 'GB...'])
-    disp(['Memory size of R3/NR3 is about ', sprintf('%4.1f',MEM), 'GB...'])
-    disp(['Will run R3/NR3 calculation into ', num2str(Loop_N), ' Loops to reduce memory load...'])
+    disp(['Memory size of R3 is about ', sprintf('%4.1f',MEM), 'GB...'])
+    disp(['Will run R3 calculation into ', num2str(Loop_N), ' Loops to reduce memory load...'])
     disp('--------------------------------------')
-    
-    % pre-allocate matrix
-    S_R3  = zeros(Ele_Max,Loop_N-1);
-    S_NR3 = zeros(Ele_Max,Loop_N-1);
 
+    % pre-allocate matrix and run the loop
+    S_R3_L = zeros(Ele_Max,Loop_N-1);
     for LN = 1:Loop_N -1 
-        N_0a = M_Ex_01(Ka_L(:,LN),:);
-        N_0b = M_Ex_01(Kb_L(:,LN),:);
-
-        N_ax = TD_M_Ex(ind_ax(:,LN),:);
-        N_bx = TD_M_Ex(ind_bx(:,LN),:);
-        N_xa = TD_M_Ex(ind_ax(:,LN),:); % ind_xa = ind_ax
-        N_xb = TD_M_Ex(ind_bx(:,LN),:); % ind_xb = ind_bx
-
-        % Response (EA)
-        S_R3(:,LN)  = (N_xa(:,Ja(:)).*N_bx(:,Jb(:)).*N_0b(:,Jc(:)).*N_0a(:,Jd(:)))*EJR';
-        S_NR3(:,LN) = (N_xb(:,Ja(:)).*N_ax(:,Jb(:)).*N_0b(:,Jc(:)).*N_0a(:,Jd(:)))*EJR';
-
-    end
-    
-    S_R3  =  S_R3(:);
-    S_NR3 = S_NR3(:);
-    
-    % deall with the last loop
-        Last_Ind = (1:mod(N3,Ele_Max))';
+        % note pre-allocate matrxi expansion to save calculation time
+        P4 = M12(Iax_R3_L(:,LN),:);
+        P3 = M12(Ibx_R3_L(:,LN),:);
+        P2 = M01( Ib_R3_L(:,LN),:);
+        P1 = M01( Ia_R3_L(:,LN),:);
         
-        Ka_End     =   Ka_L(Last_Ind,Loop_N);
-        Kb_End     =   Kb_L(Last_Ind,Loop_N);
-        ind_ax_End = ind_ax(Last_Ind,Loop_N);
-        ind_bx_End = ind_bx(Last_Ind,Loop_N);
+        S_R3_L(:,LN) = ( P4(:,Ja).*P3(:,Jb).*P2(:,Jc).*P1(:,Jd) )*EJR';
+    end    
+    S_R3_L = S_R3_L(:);
 
-        N_0a = M_Ex_01(Ka_End,:);
-        N_0b = M_Ex_01(Kb_End,:);
+    % deall with the last loop
+    Last_Ind   = (1:mod(N_R3,Ele_Max))';
+    Ia_R3_End  =  Ia_R3_L(Last_Ind,Loop_N);
+    Ib_R3_End  =  Ib_R3_L(Last_Ind,Loop_N);
+    Iax_R3_End = Iax_R3_L(Last_Ind,Loop_N);
+    Ibx_R3_End = Ibx_R3_L(Last_Ind,Loop_N);
 
-        N_ax = TD_M_Ex(ind_ax_End,:);
-        N_bx = TD_M_Ex(ind_bx_End,:);
-        N_xa = N_ax; % ind_xa = ind_ax
-        N_xb = N_bx; % ind_xb = ind_bx
-
-        % Response (EA)
-        S_R3_End  = (N_xa(:,Ja(:)).*N_bx(:,Jb(:)).*N_0b(:,Jc(:)).*N_0a(:,Jd(:)))*EJR';
-        S_NR3_End = (N_xb(:,Ja(:)).*N_ax(:,Jb(:)).*N_0b(:,Jc(:)).*N_0a(:,Jd(:)))*EJR';
+    P4 = M12(Iax_R3_End,:);
+    P3 = M12(Ibx_R3_End,:);
+    P2 = M01( Ib_R3_End,:);
+    P1 = M01( Ia_R3_End,:);
     
-    S_R3  = [ S_R3; S_R3_End];
-    S_NR3 = [S_NR3;S_NR3_End];    
+    S_R3_End = ( P4(:,Ja).*P3(:,Jb).*P2(:,Jc).*P1(:,Jd) )*EJR';
+        
+    S_R3 = [ S_R3_L; S_R3_End];    
+    
 else
-    N_0a = M_Ex_01(Ka(:),:);
-    N_0b = M_Ex_01(Kb(:),:);
-
-    N_ax = TD_M_Ex(ind_ax,:);
-    N_bx = TD_M_Ex(ind_bx,:);
-    N_xa = N_ax; % ind_xa = ind_ax
-    N_xb = N_bx; % ind_xb = ind_bx
-
-    % Response (EA)
-    S_R3  = (N_xa(:,Ja(:)).*N_bx(:,Jb(:)).*N_0b(:,Jc(:)).*N_0a(:,Jd(:)))*EJR';
-    S_NR3 = (N_xb(:,Ja(:)).*N_ax(:,Jb(:)).*N_0b(:,Jc(:)).*N_0a(:,Jd(:)))*EJR';
+    P4 = M12(Iax_R3,:);
+    P3 = M12(Ibx_R3,:);
+    P2 = M01( Ib_R3,:);
+    P1 = M01( Ia_R3,:);
+    S_R3 = ( P4(:,Ja).*P3(:,Jb).*P2(:,Jc).*P1(:,Jd) )*EJR';
 end
 
+Ea_3 = F1(Ia_R3);
+Eb_3 = F1(Ib_R3);
+Ex_3 = F2(Kx(PI_R3));
+Freq_R3 = [ Ea_3 , Eb_3 - Ea_3 , Ex_3 - Ea_3 ];
+
+%% NR3
+Iax_NR3 = Iax(PI_NR3);
+Ibx_NR3 = Ibx(PI_NR3);
+Ib_NR3  =  Kb(PI_NR3);
+Ia_NR3  =  Ka(PI_NR3);
+
+NR3 = numel(Ia_NR3);
+
+if NR3 > Ele_Max
+    % Add NaNs so I can reshape indexes
+    Padding_L = Ele_Max - mod(NR3,Ele_Max);
+    Padding_NaN = nan(Padding_L,1);
+    
+    Ia_NR3_L  = reshape([ Ia_NR3; Padding_NaN],Ele_Max,[]);
+    Ib_NR3_L  = reshape([ Ib_NR3; Padding_NaN],Ele_Max,[]);
+    Iax_NR3_L = reshape([Iax_NR3; Padding_NaN],Ele_Max,[]);
+    Ibx_NR3_L = reshape([Ibx_NR3; Padding_NaN],Ele_Max,[]);
+
+    Loop_N = size(Ia_NR3_L,2);
+    % display how many loop is going to be done
+    MEM = numel(Ia_NR3_L) * 81 * 8 / 1e9; % roughly unit in GB
+    disp('--------------------------------------')
+    disp(['Memory cut-off set at ', num2str(MEM_CutOff), 'GB...'])
+    disp(['Memory size of NR3 is about ', sprintf('%4.1f',MEM), 'GB...'])
+    disp(['Will run NR3 calculation into ', num2str(Loop_N), ' Loops to reduce memory load...'])
+    disp('--------------------------------------')
+
+    % pre-allocate matrix and run the loop
+    S_NR3_L = zeros(Ele_Max,Loop_N-1);
+    for LN = 1:Loop_N -1 
+        % note pre-allocate matrxi expansion to save calculation time
+        P4 = M12(Ibx_NR3_L(:,LN),:);
+        P3 = M12(Iax_NR3_L(:,LN),:);
+        P2 = M01( Ib_NR3_L(:,LN),:);
+        P1 = M01( Ia_NR3_L(:,LN),:);
+        
+        S_NR3_L(:,LN) = ( P4(:,Ja).*P3(:,Jb).*P2(:,Jc).*P1(:,Jd) )*EJR';
+    end    
+    S_NR3_L = S_NR3_L(:);
+
+    % deall with the last loop
+    Last_Ind    = (1:mod(NR3,Ele_Max))';
+    Ia_NR3_End  =  Ia_NR3_L(Last_Ind,Loop_N);
+    Ib_NR3_End  =  Ib_NR3_L(Last_Ind,Loop_N);
+    Iax_NR3_End = Iax_NR3_L(Last_Ind,Loop_N);
+    Ibx_NR3_End = Ibx_NR3_L(Last_Ind,Loop_N);
+
+    P4 = M12(Ibx_NR3_End,:);
+    P3 = M12(Iax_NR3_End,:);
+    P2 = M01( Ib_NR3_End,:);
+    P1 = M01( Ia_NR3_End,:);
+    
+    S_NR3_End = ( P4(:,Ja).*P3(:,Jb).*P2(:,Jc).*P1(:,Jd) )*EJR';
+        
+    S_NR3 = [ S_NR3_L; S_NR3_End];    
+    
+else
+    P4 = M12(Ibx_NR3,:);
+    P3 = M12(Iax_NR3,:);
+    P2 = M01( Ib_NR3,:);
+    P1 = M01( Ia_NR3,:);
+    
+    S_NR3 = ( P4(:,Ja).*P3(:,Jb).*P2(:,Jc).*P1(:,Jd) )*EJR';
+end
+
+Ea_6 = F1(Ia_NR3);
+Eb_6 = F1(Ib_NR3);
+Ex_6 = F2(Kx(PI_NR3));
+Freq_NR3 = [ Ea_6 , Ea_6 - Eb_6 , Ex_6 - Eb_6 ];
+
 %% Prep for export mode index
-I_R1  = [Ib(:),Ib(:),Ia(:),Ia(:)]; % GB
-I_R2  = [Ib(:),Ia(:),Ib(:),Ia(:)]; % SE
-I_R3  = [Ka(:),Kx(:),Kb(:),Ka(:)]; % EA
+Ind_R1  = [Ib,Ib,Ia,Ia]; % GB
+Ind_R2  = [Ib,Ia,Ib,Ia]; % SE
+Ind_R3  = [Ka,Kx,Kb,Ka]; % EA
 
-I_NR1 = [Ib(:),Ib(:),Ia(:),Ia(:)]; % GB
-I_NR2 = [Ia(:),Ib(:),Ib(:),Ia(:)]; % SE
-I_NR3 = [Kb(:),Kx(:),Kb(:),Ka(:)]; % EA
-
-%% Generate List of interaction Frequencies
-Ea_12 = F1(Ia(:));
-Eb_12 = F1(Ib(:));
-
-Freq_R1  = [-Ea_12, zeros(size(Ea_12)), Eb_12];
-Freq_R2  = [-Ea_12, Eb_12 - Ea_12     , Eb_12];
-Freq_NR1 = [ Ea_12, zeros(size(Ea_12)), Eb_12];
-Freq_NR2 = [ Ea_12, Ea_12 - Eb_12     , Ea_12]; % This is a interesting term! only contribute to diagonal!
-
-Ea_3 = F1(Ka(:));
-Eb_3 = F1(Kb(:));
-Ex_3 = F2(Kx(:));
-
-Freq_R3  = [-Ea_3, Eb_3 - Ea_3, Ex_3 - Ea_3];
-Freq_NR3 = [ Ea_3, Ea_3 - Eb_3, Ex_3 - Eb_3];
+Ind_NR1 = [Ib,Ib,Ia,Ia]; % GB
+Ind_NR2 = [Ia,Ib,Ib,Ia]; % SE
+Ind_NR3 = [Kb,Kx,Kb,Ka]; % EA
 
 %% Construct sparse matrix version of Responses
-SparseMax = max([max(Ea_12), max(Ex_3 - Ea_3), max(FreqRange)]); 
+SparseMax = max([max(F1), max(F2(Kx(:)) - F1(Ka(:))), max(FreqRange)]); 
 
-R1  = sparse(Ea_12,       Eb_12,S_R1 ,SparseMax,SparseMax);
-R2  = sparse(Ea_12,       Eb_12,S_R2 ,SparseMax,SparseMax);
-R3  = sparse(Ea_3 ,Ex_3 - Ea_3 ,S_R3 ,SparseMax,SparseMax);
-NR1 = sparse(Ea_12,       Eb_12,S_NR1,SparseMax,SparseMax);
-NR2 = sparse(Ea_12,       Ea_12,S_NR2,SparseMax,SparseMax);
-NR3 = sparse(Ea_3 ,Ex_3 - Eb_3 ,S_NR3,SparseMax,SparseMax);
-
-%whos %% debug
+R1  = sparse( Freq_R1(:,1), Freq_R1(:,3),S_R1 ,SparseMax,SparseMax);
+R2  = sparse( Freq_R2(:,1), Freq_R2(:,3),S_R2 ,SparseMax,SparseMax);
+R3  = sparse( Freq_R3(:,1), Freq_R3(:,3),S_NR3,SparseMax,SparseMax);
+NR1 = sparse(Freq_NR1(:,1),Freq_NR1(:,3),S_NR1,SparseMax,SparseMax);
+NR2 = sparse(Freq_NR2(:,1),Freq_NR2(:,3),S_NR2,SparseMax,SparseMax);
+NR3 = sparse(Freq_NR3(:,1),Freq_NR3(:,3),S_NR3,SparseMax,SparseMax);
 
 %% Output
 MinF = min(FreqRange);
@@ -256,7 +326,7 @@ Grid.NR3 = full(NR3(MinF:MaxF,MinF:MaxF)); % [Pumpx Probe]
 
 Int.R1  = S_R1;
 Int.R2  = S_R2;
-Int.R3  = S_R3;
+Int.R3  = S_NR3;
 Int.NR1 = S_NR1;
 Int.NR2 = S_NR2;
 Int.NR3 = S_NR3;
@@ -268,9 +338,9 @@ Freq.NR1 = Freq_NR1;
 Freq.NR2 = Freq_NR2;
 Freq.NR3 = Freq_NR3;
 
-Index.R1  = I_R1;
-Index.R2  = I_R2;
-Index.R3  = I_R3;
-Index.NR1 = I_NR1;
-Index.NR2 = I_NR2;
-Index.NR3 = I_NR3;
+Index.R1  = Ind_R1;
+Index.R2  = Ind_R2;
+Index.R3  = Ind_R3;
+Index.NR1 = Ind_NR1;
+Index.NR2 = Ind_NR2;
+Index.NR3 = Ind_NR3;

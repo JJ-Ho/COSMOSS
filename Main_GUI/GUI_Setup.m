fe094f834bae97084fda5179075638cc4a4b231f
Jia-Jung Ho <jhoatgit@gmail.com>
2017-10-01 23:12:50 -0500

    Organize COSMOSS GUI and add options Add vectors into TCO structure parameter table

--- Main_GUI/GUI_Setup.m
+++ Main_GUI/GUI_Setup.m
@@ -1,4 +1,9 @@
 function GUI_Setup(app)
+% Setup the Model structure list
+[~,StructureModelList,~] = StructureModel(0);
+app.ListBox_Model.Items = StructureModelList;
+app.ListBox_Model.ItemsData = 1:length(StructureModelList);
+
 % Setup initial values of the experimental setups
 Exp_1D = zeros(2,3);
 Exp_1D(1,:) = 90;
@@ -11,3 +16,8 @@ Exp_2D(1,:) = 90;
 app.UITable_2D.Data = Exp_2D;
 app.UITable_2D.ColumnWidth = {44,44,40,41,40};
 app.UITable_2D.RowName = {'Ang.';'Pol.'};
+
+% Setup color map list
+[~,CMap_List] = SelectColormap(0);
+app.DropDown_ColorMap.Items = CMap_List;
+app.DropDown_ColorMap.ItemsData = 1:length(CMap_List);

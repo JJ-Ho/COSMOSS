3783cde06c633015584afe78944cf88f7788fd31
JJHo <jhoatgit@gmail.com>
2020-02-04 00:42:11 +0800

    Packaged as Matlab App

---
+++ MoleculeConstruction/@StructureData/SD_1ExH 2.m
@@ -0,0 +1,72 @@
+function obj_1ExH = SD_1ExH(obj_SD)
+%% OneExcitonH generation method of StructureData class
+%  
+% Given StructureData, this script deal with both the diagonal (LocFreq)
+% and off-diagonal (Beta) elements of a one Exciton Hamiltonian using the
+% GUI_Inputs.
+% Copyright Jia-Jung Ho, 2013-2020
+
+%% GUI Inputs parser
+GUI_Inputs = obj_SD.GUI_Inputs;
+
+% Turn Output from Read GUI to cell array
+GUI_Inputs_C      = fieldnames(GUI_Inputs);
+GUI_Inputs_C(:,2) = struct2cell(GUI_Inputs);
+GUI_Inputs_C      = GUI_Inputs_C';
+
+INPUT = inputParser;
+INPUT.KeepUnmatched = true;
+
+defaultNLFreq       = 1716;
+defaultAnharm       = 20;
+defaultLFreq        = 1604;
+defaultL_Index      = 'None';
+defaultLocFreqType  = 1;
+defaultCouplingType = 'TDC';
+defaultBeta_NN      = 0.8; % 0.8 cm-1 according to Lauren's PNAS paper (doi/10.1073/pnas.1117704109); that originate from Min Cho's paper (doi:10.1063/1.1997151)
+
+addOptional(INPUT,'NLFreq'      ,defaultNLFreq         );
+addOptional(INPUT,'Anharm'      ,defaultAnharm         );
+addOptional(INPUT,'LFreq'       ,defaultLFreq          );
+addOptional(INPUT,'L_Index'     ,defaultL_Index        );
+addOptional(INPUT,'LocFreqType' ,defaultLocFreqType    );
+addOptional(INPUT,'CouplingType',defaultCouplingType   );
+addOptional(INPUT,'Beta_NN'     ,defaultBeta_NN        );
+
+parse(INPUT,GUI_Inputs_C{:});
+
+% Reassign Variable names
+NLFreq       = INPUT.Results.NLFreq;
+Anharm       = INPUT.Results.Anharm;
+LFreq        = INPUT.Results.LFreq;
+L_Index      = INPUT.Results.L_Index;
+LocFreqType  = INPUT.Results.LocFreqType;
+CouplingType = INPUT.Results.CouplingType;
+Beta_NN      = INPUT.Results.Beta_NN;
+
+%% Update Anharmonicity
+obj_1ExH           = SD_Copy(obj_SD);
+obj_1ExH.LocAnharm = ones(obj_SD.Nmodes,1).*Anharm;
+
+%% Deal with local mode frequencies and their labeling
+LocFreq = ones(obj_SD.Nmodes,1).*NLFreq;
+
+% Apply Jansen map if needed
+if strcmp(LocFreqType,'Jensen Map')
+    [~,dF_Jansen] = Coupling_Jansen(obj_SD);
+    LocFreq = LocFreq + dF_Jansen;
+end
+
+% Apply isotope labeling
+if ~isempty(L_Index)
+    LocFreq(L_Index) = LFreq;  
+end
+
+obj_1ExH.LocFreq = LocFreq;
+
+%% Generate Off-Diagonal Elements (Beta)
+obj_1ExH.Beta = Coupling(obj_1ExH,CouplingType,Beta_NN);
+
+
+
+
---
+++ SpecFunc/MolecularResponse/H_handler 2.m
@@ -0,0 +1,188 @@
+function Output= H_handler(SData,Main_GUI_Inputs,ExMode)
+% this function handle the One exciton Hamiltonian in the StrutureData with
+% the following tasks:
+%   1. Diagonal disorder
+%   2. Off-Diagonal disorder
+%   3. Generate Two-Exciton part if needed
+%   4. Diagnalization
+
+%% Inputs parser
+% Turn Output from Read GUI to cell array
+GUI_Inputs_C      = fieldnames(Main_GUI_Inputs);
+GUI_Inputs_C(:,2) = struct2cell(Main_GUI_Inputs);
+GUI_Inputs_C      = GUI_Inputs_C';
+
+INPUT = inputParser;
+INPUT.KeepUnmatched = true;
+
+defaultSampling     = 0;
+defaultDD_FWHM      = 0;
+defaultODD_FWHM     = 0;
+defaultP_FlucCorr   = 100;
+
+addOptional(INPUT,'Sampling'    ,defaultSampling);
+addOptional(INPUT,'DD_FWHM'     ,defaultODD_FWHM);
+addOptional(INPUT,'ODD_FWHM'    ,defaultODD_FWHM);
+addOptional(INPUT,'P_FlucCorr'  ,defaultP_FlucCorr);
+
+parse(INPUT,GUI_Inputs_C{:});
+
+Sampling     = INPUT.Results.Sampling;
+DD_FWHM      = INPUT.Results.DD_FWHM;
+ODD_FWHM     = INPUT.Results.ODD_FWHM;
+P_FlucCorr   = INPUT.Results.P_FlucCorr;
+
+%% reassign variable names
+OneExH       = SData.OneExH;
+Beta         = SData.Beta;
+N            = SData.Nmodes;
+LocFreq      = SData.LocFreq;
+LocAnharm    = SData.LocAnharm;
+
+%% check if apply random sampling
+if Sampling
+    DD_std  = DD_FWHM./(2*sqrt(2*log(2)));
+    ODD_std = ODD_FWHM/(2*sqrt(2*log(2)));
+else
+    DD_std  = 0;
+    ODD_std = 0;
+end
+
+%% Diagonal/Off-Diaginal disorder if any
+P_FlucCorr = P_FlucCorr/100; % turn percentage to number within 0~1
+
+% diagonal disorder
+Correlation_Dice = rand;
+if Correlation_Dice < P_FlucCorr
+    dF_DD = DD_std.*(randn(1,1).*ones(N,1));
+else 
+    dF_DD = DD_std.*randn(N,1); 
+end
+dF_DD = [0;dF_DD]; % add the zero exciton part
+
+% Off diagonal disorder
+dBeta   = ODD_std*randn(N);
+dBeta   = (dBeta + dBeta')./2; % symetrize
+dBeta   = blkdiag(0,dBeta);
+
+OneExH = OneExH + bsxfun(@times,eye(N+1),dF_DD) + dBeta;
+
+%% Generate Two Exciton block diag part of full Hamiltonianif needed (TwoExOvertoneH & TwoExCombinationH)
+TEDIndexBegin = [];
+TEDIndexEnd   = [];
+
+if strcmp(ExMode,'TwoEx')
+    StatesNum = (N+2)*(N+1)/2; 
+
+    %- Pre-allocate the total matrix size
+    TwoExPart = zeros(StatesNum-1-N,StatesNum-1-N);
+
+    %- Two Exciton Overtone Hamiltonian, TwoExOvertoneH
+    TwoExPart(1:N,1:N) = diag(2*LocFreq-LocAnharm);
+
+    %- Two Exciton Combination Hamiltonian, TwoExCombinationH
+    NumOfElementInBolcks = N-1:-1:1;
+    TEDIndexEnd   = zeros(N-1,1);
+    TEDIndexBegin = zeros(N-1,1);
+
+    for L=1:N-1
+        TEDIndexEnd(L)   = sum(NumOfElementInBolcks(1:L)); % TED =TwoExCombination
+        TEDIndexBegin(L) = TEDIndexEnd(L) - NumOfElementInBolcks(L) + 1;
+    end
+
+    % Shift index for accomdation of TwoExOvertoneH
+    TEDIndexBegin = TEDIndexBegin+N;
+    TEDIndexEnd   = TEDIndexEnd+N;
+
+    % Off-block-Diagonal, Lower triangular part, of TwoExCombinationH
+    for k2=1:N-1
+        for k1=k2+1:N-1
+
+            TempOffDiagMatrix = zeros(N-k1,N-k2);
+            TempOffDiagMatrix(:,k1-k2) = Beta(k1+1:end,k2);
+            TempOffDiagMatrix(:,k1-k2+1:end) = eye(N-k1)*Beta(k1,k2);
+
+            TwoExPart(TEDIndexBegin(k1):TEDIndexEnd(k1),TEDIndexBegin(k2):TEDIndexEnd(k2))...
+                = TempOffDiagMatrix;
+        end
+    end
+
+    % Off-block-Diagonal, Upper triangular part of TwoExCombinationH
+    for k2=2:N-1
+        for k1=1:k2-1
+
+            TempOffDiagMatrix = zeros(N-k1,N-k2);
+            TempOffDiagMatrix(k2-k1,:) = Beta(k2+1:end,k1);
+            TempOffDiagMatrix(k2-k1+1:end,:) = eye(N-k2)*Beta(k2,k1);
+
+            TwoExPart(TEDIndexBegin(k1):TEDIndexEnd(k1),TEDIndexBegin(k2):TEDIndexEnd(k2))...
+                = TempOffDiagMatrix;
+        end
+    end
+
+    % --- Note ----------------------------------------------------------------
+    % To run this:
+    % TwoExCombinationH = TwoExCombinationH + TwoExCombinationH';  
+    % is slower than running another "for"
+    % --- Note ----------------------------------------------------------------
+
+    % Bolck-Diagnonal Part of of TwoExCombinationH
+    for m=1:N-1
+        TwoExPart(TEDIndexBegin(m):TEDIndexEnd(m),TEDIndexBegin(m):TEDIndexEnd(m))...
+            = bsxfun(@times,eye(N-m),LocFreq(m+1:end)+LocFreq(m)) + Beta(m+1:end,m+1:end);
+    end
+
+    %% Two Exciton Hamiltonian Cross part between TwoExOvertoneH and TwoExCombinationH
+    for n1=1:N-1
+        TempOffDiagMatrix = zeros(N,N-n1);
+        TempOffDiagMatrix(n1,:) = Beta(n1,n1+1:N).*sqrt(2);
+        TempOffDiagMatrix(n1+1:N,:) = bsxfun(@times,eye(N-n1),Beta(n1,n1+1:N).*sqrt(2));
+
+        TwoExPart(1:N,TEDIndexBegin(n1):TEDIndexEnd(n1))...
+            = TempOffDiagMatrix;
+    end
+
+    for n2=1:N-1
+        TempOffDiagMatrix = zeros(N-n2,N);
+        TempOffDiagMatrix(:,n2) = Beta(n2+1:N,n2).*sqrt(2);
+        TempOffDiagMatrix(:,n2+1:N) = bsxfun(@times,eye(N-n2),Beta(n2,n2+1:N).*sqrt(2));
+
+        TwoExPart(TEDIndexBegin(n2):TEDIndexEnd(n2),1:N)...
+            = TempOffDiagMatrix;
+    end
+
+end
+
+%% Diagonalize the full hamiltonian
+
+if strcmp(ExMode,'TwoEx')
+    FullH = blkdiag(OneExH,TwoExPart);
+else
+    FullH = OneExH;
+end 
+
+% note: the eiganvector V_Full(:,i) has been already normalized.
+[V_Full,D_Full] = eig(FullH);
+Ex_Freq = diag(D_Full);
+
+% sort eiganvalue form small to big and reorder the eiganvectors
+[Sort_Ex_Freq,Indx] = sort(Ex_Freq);
+ Sort_Ex_V          = V_Full(:,Indx);
+ 
+Sort_Ex_F1 = Sort_Ex_Freq(2:N+1);
+Sort_Ex_V1 = Sort_Ex_V(2:N+1,2:N+1);
+Sort_Ex_F2 = [];
+Sort_Ex_V2 = [];
+
+if strcmp(ExMode,'TwoEx')
+    Sort_Ex_F2 = Sort_Ex_Freq(N+2:end);
+    Sort_Ex_V2 = Sort_Ex_V(N+2:end,N+2:end);
+end 
+
+%% export
+Output.Sort_Ex_F1 = Sort_Ex_F1;
+Output.Sort_Ex_V1 = Sort_Ex_V1;
+Output.Sort_Ex_F2 = Sort_Ex_F2;
+Output.Sort_Ex_V2 = Sort_Ex_V2;
+Output.TEDIndexBegin = TEDIndexBegin;
+Output.TEDIndexEnd   = TEDIndexEnd;

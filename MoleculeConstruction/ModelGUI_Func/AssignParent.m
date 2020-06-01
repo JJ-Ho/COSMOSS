55b753b66646bc51159c8ba4592540dc4eaba1b6
Jia-Jung Ho <jhoatgit@gmail.com>
2020-06-02 03:42:11 +0800

    implement UUID for properly deleting the sub-GUIs Note: for comb2, the 'UIFigureCloseRequest(app, event)' does not kick in before it is deleted, so the sub-GUIs of comb2 won'y be close properly when directly closing COSMOSS. it will work if one close comb2 before closing COSMOSS...

--- MoleculeConstruction/ModelGUI_Func/AssignParent.m
+++ MoleculeConstruction/ModelGUI_Func/AssignParent.m
@@ -1,16 +1,17 @@
 function AssignParent(app,ParentInfo)
 switch ParentInfo{1}
-    case 'COSMOSS'
-        app.Parent      = ParentInfo{2};
-        app.fileChooser = ParentInfo{2}.fileChooser; % use the selected fileChooser in COSMOSS
+    case 'COSMOSS' %% Need to clean up UUID field later!
+        app.UIFigure.Tag = ParentInfo{2}.UUID;
+        app.fileChooser  = ParentInfo{2}.fileChooser; % use the selected fileChooser in COSMOSS
         disp('Running sub-GUI from COSMOSS...')
-    case 'Comb2'
-        app.Parent    = ParentInfo{2};
-        app.fileChooser = ParentInfo{2}.fileChooser; % use the selected fileChooser in COSMOSS
-        app.CombOrder = ParentInfo{3};
+    case 'Comb2' 
+        app.UIFigure.Tag = ParentInfo{2}.UUID;
+        app.fileChooser  = ParentInfo{2}.fileChooser; % use the selected fileChooser in COSMOSS
+        app.CombOrder    = ParentInfo{3};
         disp('Running sub-GUI from Comb2...')
     case 'StandAlong'
-        app.Parent.GUI_Tag = 'None';
-        app.fileChooser    = DefaultFileChooser;
+        app.fileChooser = DefaultFileChooser;
         disp('Running Model GUI in stand alone mode...')
+    otherwise
+        disp([ParentInfo{1}, ' is not defined...'])
 end

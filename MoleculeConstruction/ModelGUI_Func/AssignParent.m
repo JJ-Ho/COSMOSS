5383cf0c941ed1f09ad00c525ff359d5bdd8b6a3
Jia-Jung Ho <jhoatgit@gmail.com>
2020-06-02 20:24:14 +0800

    Bug fixed, renamed the parent GUI handle in the children as "ParentGUI"

--- MoleculeConstruction/ModelGUI_Func/AssignParent.m
+++ MoleculeConstruction/ModelGUI_Func/AssignParent.m
@@ -1,15 +1,18 @@
 function AssignParent(app,ParentInfo)
 switch ParentInfo{1}
     case 'COSMOSS' %% Need to clean up UUID field later!
+        app.ParentGUI    = ParentInfo{2};
         app.UIFigure.Tag = ParentInfo{2}.UUID;
         app.fileChooser  = ParentInfo{2}.fileChooser; % use the selected fileChooser in COSMOSS
         disp('Running sub-GUI from COSMOSS...')
-    case 'Comb2' 
+    case 'Comb2'
+        app.ParentGUI    = ParentInfo{2};
         app.UIFigure.Tag = ParentInfo{2}.UUID;
         app.fileChooser  = ParentInfo{2}.fileChooser; % use the selected fileChooser in COSMOSS
         app.CombOrder    = ParentInfo{3};
         disp('Running sub-GUI from Comb2...')
     case 'StandAlong'
+        app.ParentGUI   = ParentInfo{2};
         app.fileChooser = DefaultFileChooser;
         disp('Running Model GUI in stand alone mode...')
     otherwise
--- MoleculeConstruction/ModelGUI_Func/Export2Parent.m
+++ MoleculeConstruction/ModelGUI_Func/Export2Parent.m
@@ -2,13 +2,13 @@ function Export2Parent(app)
 % This function push Structure data to COSMOSS after the Model strueture data updated
 
 %% Decide if push structure to parent
-switch app.Parent.GUI_Tag
+switch app.ParentGUI.GUI_Tag
     case 'COSMOSS'
-        app.Parent.Structure = app.Structure;
-        app.Parent.UIFigure.Name = ['COSMOSS: ', app.UIFigure.Name]; % change Name of Main GUI to identify which Structural Model is using
-        app.Parent.RefreshOn % reset the all refresh tag to 1
+        app.ParentGUI.Structure = app.Structure;
+        app.ParentGUI.UIFigure.Name = ['COSMOSS: ', app.UIFigure.Name]; % change Name of Main GUI to identify which Structural Model is using
+        app.ParentGUI.RefreshOn % reset the all refresh tag to 1
     case 'Comb2'
-        app.Parent.RefreshSD % auto refresh the combined structure
+        app.ParentGUI.RefreshSD % auto refresh the combined structure
     case 'StandAlong'
         % do nothing
 end
